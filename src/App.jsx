212515479fb533132d060ffa2de2f4c3322b862d
Srilochan <srilochan7@gmail.com>
2025-02-09 10:20:45 +0530

    added some pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React,{ useState } from 'react'
 import "./index.css"; // Ensure Tailwind is applied globally
-
+import Hero from './components/hero'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">Tailwind is Working!</h1>
-    </div>
+    <>
+    <Hero/>
+    </>
   );
 }
 
