c4537de3348f052597858eb449021d86f21e8bb7
MPreethika <manepallypreethika@gmail.com>
2025-07-27 20:27:38 +0530

    replaced model

--- src/components/pages/mock.jsx
+++ src/components/pages/mock.jsx
@@ -20,7 +20,12 @@ const MockInterview = () => {
 
   // Initialize the model
   const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  //const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  //const model = genAI.getGenerativeModel({ model: "models/gemini-1.5-pro" });
+  const model = genAI.getGenerativeModel({ model: "models/gemini-1.5-flash" });
+
+
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
